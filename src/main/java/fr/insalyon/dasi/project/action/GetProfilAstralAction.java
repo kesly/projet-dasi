025b1d336b347784332549dba742e144e8da7023
ndb_git <nielsdebarbanson@hotmail.fr>
2020-06-15 18:06:04 +0200

    wip vue profil astral

---
+++ src/main/java/fr/insalyon/dasi/project/action/GetProfilAstralAction.java
@@ -0,0 +1,32 @@
+package fr.insalyon.dasi.project.action;
+
+import fr.insalyon.dasi.td1.metier.modele.Client;
+import fr.insalyon.dasi.td1.metier.service.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class AuthentifierClientAction extends Action{
+
+    @Override
+    public void executer(HttpServletRequest request) {
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        Service service = new Service();
+        Client client = service.getClientByAuthentication(login, password);
+
+        request.setAttribute("client", client);
+
+        // Gestion de la Session: ici, enregistrer l'ID du Client authentifié
+        HttpSession session = request.getSession();
+        if (client != null) {
+            session.setAttribute("idClient", client.getId());
+        }
+        else {
+            session.removeAttribute("idClient");
+        }
+
+    }
+}
